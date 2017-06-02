4dd9741aef71324173b16a63c8b49a614a2c9e9e
Franco <fpalossi@gmail.com>
2017-06-02 20:04:36 -0300

    hshd:

--- app/services/reclamosServices.js
+++ app/services/reclamosServices.js
@@ -21,6 +21,33 @@ app.service(
 	            .success(function (data) {
 	                
 	                 //hago algo si todo va bien, por defecto, resuevlo el deferred object 
+	                 //debugger;
+	                 var alldata = arguments[3].data;
+	                 
+	                 $http({
+	                method: 'POST',
+	                url: 'http://200.80.240.114:8000/asunto/crear/',
+	                headers: {
+	                  //si necesitas mandas headers tambien  
+	                },
+	                data: {
+						        "id": 1,
+						        "descripcion": "asunto 1",
+						        "apellido": "Colella",
+						        "nombre": "Eloy",
+						        "dni": 33852292,
+						        "telefono": "3364541000",
+						        "email": "eloycolella@gmail.com",
+						        "domicilio": "Alem 1333 4-4",
+						        "causante": "Causante 1",
+						        "tipo_rechazo": null,
+						        "fecha_rechazo": null,
+						        "fecha_aceptacion": "2017-05-30",
+						        "fecha_alta": "2017-05-30"
+						    }
+
+	            })
+
 	                deferred.resolve(data);
 	            }).error(function (data) {
 	            	//hago algo con el erroro aca
